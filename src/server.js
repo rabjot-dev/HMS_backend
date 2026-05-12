85adb46d4d72f28b458b01e6f374e7f2886ddcad
rabjot-dev <308236@ust.com>
2026-05-12 12:14:00 +0530

    initialized server.js

---
+++ src/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const connnectDB = require("./config/db");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect to database
+const startServer = async () => {
+  try {
+    await connnectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to connect to the database", error);
+    process.exit(1); // Exit the process with an error code
+  }
+};
+
+startServer();
+    console.log("Database connected successfully");
