d2046f185b58adcf9dc74d729393228f28b7dea4
fathimathselma8 <308255@ust.com>
2026-06-24 17:46:46 +0530

    server.js modified

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
-    await connnectDB();
+    await connectDB();
     console.log("Database connected successfully");
 
     await seedAdmin();
